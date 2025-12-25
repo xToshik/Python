d86a4f30b6e3f91459cbe75a111412b2cda0ab4c
xToshik <thecrabstarname@gmail.com>
2025-12-25 12:26:54 +0900

    Олимпиада

---
+++ all (4).py
@@ -0,0 +1,39 @@
+#1
+# n = [6,2,5,4,2,5,6]
+# mas = []
+# for i in range(len(n)):
+#     for j range(len(n)):
+#         if n[j] < n[i]:
+#             mas[i] = max(mas[i], mas[j] + 1)
+# print(mas)
+# def f(n):
+#     if n[0] > n[1]:
+#         return f(n[1:])
+#     if n[1] >= n[2] and n[2] <=:
+#         del n[1]
+#         return f(n)
+#     return n
+# n = [6,2,5,4,2,5,6]
+# print(f(n))
+#2
+# n = int(input())
+# spisok = []
+# if 1 <= n <= 1000:
+#     for i in range(0, n):
+#         a = int(input())
+#         spisok.append(a)
+#         for j in range(i):
+            
+#3
+# num = '1050'
+# r_num = []
+# flag = False
+# for i in reversed(num):
+#     if i == '0' and flag == False:
+#         continue
+#     else:
+#         flag = True
+#         r_num.append(i)
+# print(r_num)
+
+    
